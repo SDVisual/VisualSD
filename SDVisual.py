b5deb4c3a760b9c67f87adcdc0a170196a453534
SDVisual <164010355+SDVisual@users.noreply.github.com>
2024-03-21 13:54:38 +0200

    Update SDVisual.py

--- SDVisual.py
+++ SDVisual.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 StockInfo = {}
 StockInfo_df = pd.DataFrame()
 
-APP_NAME = "Stock Data Visualization"
+APP_NAME = "Stock Data Interactive Visualization"
 
 st.set_page_config(
     page_title=APP_NAME,
