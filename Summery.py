a75e8ad4eec871269883870ca04f5490b27e3b8e
SDVisual <164010355+SDVisual@users.noreply.github.com>
2024-03-31 13:54:45 +0300

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -184,7 +184,7 @@ col1, col2, col3 = st.columns([0.45, 0.45, 0.05])  # Adjust the width ratio of c
 with col1:
 
     st.subheader(f'Stock Summary -  {StockInfo["shortName"]}')
-     st.markdown(f"<h1 style='color:blue; font-size: smaller;'>{StockInfo['sector']}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='color:blue; font-size: smaller;'>{StockInfo['sector']}</h1>", unsafe_allow_html=True)
     st.markdown(f"<h1 style='color:blue; font-size: smaller;'>{StockInfo['industry']}</h1>", unsafe_allow_html=True)
     
     # # Display the industry description
