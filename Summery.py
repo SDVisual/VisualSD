5ef16d081e7a45fdc72f7f6c661c62bef4a79af8
SDVisual <164010355+SDVisual@users.noreply.github.com>
2024-03-31 16:21:59 +0300

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -272,9 +272,10 @@ with col1:
 
 # Column 2: *****************************      Candlestick chart         ***************************
 with col2:
-    # Centered subheader with dates
-    st.markdown("<h3 style='text-align: center;'>Stock Chart For Dates: {} to {}</h3>".format(
-        start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")), unsafe_allow_html=True)
+    
+    # # Centered subheader with dates
+    # st.markdown("<h3 style='text-align: center;'>Stock Chart For Dates: {} to {}</h3>".format(
+    #     start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")), unsafe_allow_html=True)
 
     if df_ticker.empty:
         st.warning(f"No data found for {ticker} in the selected date range.")
