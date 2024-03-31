cb5e63ae163dc38c1e3002c2aa967aae1926f17f
SDVisual <164010355+SDVisual@users.noreply.github.com>
2024-03-31 13:35:33 +0300

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -185,7 +185,7 @@ with col1:
 
     st.subheader(f'Stock Summary -  {StockInfo["shortName"]}')
     st.markdown(f"<h1 style='color:blue; font-size: smaller;'>{StockInfo['sector']}</h1>", unsafe_allow_html=True)
-    st.markdown(f"<h1 style='color:blue; font-size: smaller;'>{StockInfo['industryDisp']}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='color:blue; font-size: smaller;'>{StockInfo['sectorDisp']}</h1>", unsafe_allow_html=True)
 
 
     st.markdown("## \n\n\n")  # Add an empty line
