37a6e820520a44f7f46d3e7b099c6c7d140af821
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-04-16 14:23:56 +0300

    Update 2_Income_Statements.py

--- pages/2_Income_Statements.py
+++ pages/2_Income_Statements.py
@@ -49,16 +49,15 @@ selected_symbol = st.session_state.valid_tickers[selected_ticker_index]
 # Check if the entered symbol is empty or consists only of whitespace characters
 if not new_symbol or new_symbol.isspace():
     new_symbol = DEFAULT_SYMBOL
-else:
-    if new_symbol in st.session_state.valid_tickers:
-        st.warning(f"'{new_symbol}' is already in Symbols List - Clear Text")
 
 
 # Check if the entered symbol is valid
 historical_data = yf.Ticker(new_symbol).history(period='1d')
+income_statement = yf.Ticker(new_symbol).income_stmt
 
-if new_symbol != selected_symbol and historical_data.empty:
-    st.error("Invalid symbol. Please enter a valid symbol.")
+if new_symbol != DEFAULT_SYMBOL and historical_data.empty or income_statement.empty:
+    st.error("Invalid symbol. Please enter Only Stocks symbols.")
+
 else:
     if new_symbol not in st.session_state.valid_tickers:
         st.session_state.valid_tickers.append(new_symbol)
@@ -89,24 +88,28 @@ IncomeStatementQuarterly = yf.Ticker(ticker).quarterly_income_stmt
 # Default to annual income statement
 income_statement = income_statementYear
 
-# color_code = "#0ECCEC"
-# font_size = "25px"  # You can adjust the font size as needed
-#
-# # Render subheader with customized font size and color
-# st.markdown(f'<h2 style="color:{color_code}; font-size:{font_size}">{StockInfo["shortName"]}</h2>', unsafe_allow_html=True)
+
 
 
 symbol = StockInfo["shortName"]
 color_code = "#0ECCEC"  # Color for the symbol
 
 # Combine st.write() with HTML-styled header
-st.write(f'<span style="color:white; font-size:30px;">Income Statement - </span>'
+st.write(f'<span style="font-size:30px;">Income Statement - </span>'
          f'<span style="color:{color_code}; font-size:30px;">{symbol}</span>', unsafe_allow_html=True)
+
+# # Combine st.write() with HTML-styled header
+# st.write(f'<span style= font-size:30px;">Income Statement - </span>'
+#          f'<span style="color:{color_code}; font-size:30px;">{symbol}</span>', unsafe_allow_html=True)
 
 
 
 
 st.write("")
+
+
+
+
 
 
 # Checkbox to select between annual and quarterly
@@ -233,8 +236,10 @@ with col1:
     if not data.empty:
         fig = go.Figure()
 
+
+
         # Define colors for each quarter
-        colors = ['blue', 'red', 'yellow', 'purple']  # Add more colors if needed
+        colors = ['#0080ff', '#5483b3', '#7da0ca', '#c1e8ff']  # Add more colors if needed
 
         # Create traces for each quarter or year based on checkbox value
         for i, date in enumerate(data.index):
@@ -250,7 +255,7 @@ with col1:
                 text=[f"{value:.2f}%" for value in data.loc[date]],
                 textposition='auto',
                 insidetextanchor='start',
-                marker=dict(color=colors[i % len(colors)], line=dict(width=2, color='black')),
+                marker=dict(color=colors[i % len(colors)], line=dict(width=1, color='black')),
                 insidetextfont=dict(size=15),
             ))
 
@@ -268,7 +273,7 @@ with col1:
             title_xanchor='center',
             title_yanchor='top',
             font=dict(size=15),
-            legend=dict(orientation="h", yanchor="bottom", y=1.07, xanchor="center", x=0.45),
+            legend=dict(orientation="h", yanchor="bottom", y=1.07, xanchor="center", x=0.45, font=dict(size=13)),
             # Center the legend
         )
 
@@ -277,7 +282,15 @@ with col1:
         st.write("Income statement is empty.")
 
 
+
+
+
 st.subheader(f"revenues & expenses ")
+
+
+
+
+
 st.write("")
 st.write("")
 col1, col2, col3 = st.columns([0.35, 0.35, 0.4])  # Adjust the width ratio of col1 and col2 as needed
@@ -294,7 +307,7 @@ with col1:
         fig = go.Figure()
 
         # Define colors for each trace
-        colors = ['blue', 'yellow', 'green']  # Add more colors if needed
+        colors = ['blue', '#0080ff', '#c1e8ff']  # Add more colors if needed
 
         # Create traces outside the loop
         for i, metric in enumerate(['Total Revenue', 'Operating Income', 'Net Income']):
@@ -324,14 +337,14 @@ with col1:
             yaxis=dict(title='Amount (M$)'),
             width=300,
             height=500,
-            title_text=f'Company Revenues YoY' + (" (By Quarters)" if is_quarterly else " (By Years)"),
+            title_text=f'Company Revenues' + (" QoQ" if is_quarterly else " YoY"),
             # Update title based on checkbox value
             title_x=0.5,
             title_y=0.98,
             title_xanchor='center',
             title_yanchor='top',
             font=dict(size=15),
-            legend=dict(orientation="h", yanchor="bottom", y=1.06, xanchor="center", x=0.45),  # Center the legend
+            legend=dict(orientation="h", yanchor="bottom", y=1.06, xanchor="center", x=0.45, font=dict(size=13)),  # Center the legend
         )
 
         st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
@@ -350,7 +363,7 @@ with col2:
         fig_expenses = go.Figure()
 
         # Define colors for each trace
-        colors_expenses = ['red', 'blue', 'lightblue']
+        colors_expenses = ['red', '#cd5c5c', '#fa8072']
 
         # Create traces outside the loop
         for i, metric in enumerate(
@@ -378,7 +391,7 @@ with col2:
             yaxis=dict(title='Amount (M$)'),
             width=300,  # Use the same width as in the first chart
             height=500,  # Use the same height as in the first chart
-            title_text=f'Company Expenses YoY' + (" (By Quarters)" if is_quarterly else " (By Years)"),
+            title_text=f'Company Expenses' + (" QoQ" if is_quarterly else " YoY"),
             # Update title based on checkbox value
             title_x=0.5,
             title_y=0.98,
@@ -414,6 +427,46 @@ percentage_change_df.iloc[:, 0] = percentage_change_df.iloc[:, 0].fillna(0)
 data_percentage_change_df = percentage_change_df.loc[
     ['Total Revenue', 'Gross Profit', 'Operating Income', 'Net Income']].transpose()
 
+# st.write(income_statement)
+
+
+
+
+
+data = income_statement.loc[['Total Revenue', 'Gross Profit', 'Operating Income', 'Net Income']].transpose()
+# Convert string values to numeric
+data = pd.DataFrame(data).apply(lambda x: x.str.replace(',', '').astype(float))
+
+# # Calculate CAGR for each column
+# cagr_results = {}
+# for column in data.columns:
+#     # Check if start and end values have different signs
+#     if (data[column].iloc[0] < 0 and data[column].iloc[-1] > 0) or (
+#             data[column].iloc[0] > 0 and data[column].iloc[-1] < 0):
+#         # Calculate CAGR for the current metric with directional adjustment
+#         start_value = abs(data[column].iloc[0])
+#         end_value = abs(data[column].iloc[-1])
+#         num_periods = len(data) - 1
+#         cagr = ((end_value / start_value) ** (1 / num_periods)) - 1
+#     else:
+#         # Regular CAGR calculation
+#         start_value = data[column].iloc[0]
+#         end_value = data[column].iloc[-1]
+#         num_periods = len(data) - 1
+#         cagr = ((end_value / start_value) ** (1 / num_periods)) - 1
+#
+#     # Store CAGR result
+#     cagr_results[column] = cagr
+#
+# # Display the results in Streamlit
+# st.title("CAGR Calculation")
+# st.write("CAGR for each element:")
+# # st.write(cagr_results)
+# for column, cagr in cagr_results.items():
+#     st.write(f"{column}: {cagr:.2%}")
+
+
+
 
 
 
@@ -421,23 +474,44 @@ data_percentage_change_df = percentage_change_df.loc[
 with col1:
     # # Add title in the middle with smaller font size
     # st.markdown("<h2 style='text-align: left; color: white'>Company Growth Trend</h2>", unsafe_allow_html=True)
-    st.subheader(f"Growth Trend")
+    st.subheader(f"Growth Trend {'QoQ' if is_quarterly else 'YoY'}")
+
 
     st.write("")
     # Use Streamlit's columns layout manager to display charts side by side
     col1, col2 = st.columns(2)
+    # Define colors for each quarter
+    # colors = ['#0080ff', '#5483b3', '#7da0ca', '#c1e8ff']  # Add more colors if needed
+
+
 
     # Define colors for each metric
-    line_colors = ['blue', 'red', 'green', 'orange']
+    line_colors = ['blue', '#0080ff']
+
+
 
     # Create a new figure for the line chart
     line_fig = go.Figure()
 
     # Define the list of metrics
-    metrics = ['Total Revenue', 'Gross Profit', 'Operating Income', 'Net Income']
+    metrics = ['Total Revenue', 'Gross Profit']
 
     # Iterate over each metric and create a chart
     for metric, col in zip(metrics, [col1, col2]):
+        # Check if start and end values have different signs
+        if (data[metric].iloc[0] < 0 and data[metric].iloc[-1] > 0) or (data[metric].iloc[0] > 0 and data[metric].iloc[-1] < 0):
+            # Calculate CAGR for the current metric
+            start_value = abs(data[metric].iloc[0])
+            end_value = abs(data[metric].iloc[-1])
+            num_periods = len(data) - 1
+            cagr = ((end_value / start_value) ** (1 / num_periods)) - 1
+        else:
+            # Regular CAGR calculation
+            start_value = data[metric].iloc[0]
+            end_value = data[metric].iloc[-1]
+            num_periods = len(data) - 1
+            cagr = ((end_value / start_value) ** (1 / num_periods)) - 1
+
         # Create a new chart for the current metric
         with col:
             if not data_percentage_change_df.empty:
@@ -446,53 +520,60 @@ with col1:
 
                 # Add trace for the current metric
                 fig.add_trace(go.Scatter(
-                    x=pd.to_datetime(data_percentage_change_df.index).strftime(
-                        '%Y-%m-%d' if is_quarterly else '%Y-%m'),
+                    x=pd.to_datetime(data_percentage_change_df.index).strftime('%Y-%m-%d' if is_quarterly else '%YY'),
                     y=data_percentage_change_df[metric],
                     mode='lines',
                     name=metric,
-                    line=dict(color=line_colors[metrics.index(metric)]),  # Assign color from the list
+                    line=dict(color=line_colors[metrics.index(metric)], width=3)  # Assign color from the list
                 ))
 
                 # Add annotations for each point on the line
                 for index, row in data_percentage_change_df.iterrows():
                     index_dt = pd.to_datetime(index)  # Convert index to datetime object
                     fig.add_annotation(
-                        x=index_dt.strftime('%Y-%m-%d' if is_quarterly else '%Y-%m'),
+                        x=index_dt.strftime('%Y-%m-%d' if is_quarterly else '%YY'),
                         # Use specific x-value for each data point
                         y=row[metric],  # y-coordinate of the annotation
                         text=f"{row[metric]:.2f}%",  # text to display (format as needed)
                         showarrow=False,  # don't show an arrow
-                        font=dict(color='white'),  # color of the annotation text
                         xanchor='right',  # anchor point for x-coordinate
                         yanchor='middle',  # anchor point for y-coordinate
                         align='left',  # alignment of the annotation text
+                        bgcolor='yellow',
                         xshift=5,  # horizontal shift of the annotation
                         yshift=0,  # vertical shift of the annotation
                     )
-
+                    # Define the color based on CAGR value
+                color = 'red' if cagr < 0 else 'yellow'  # Change to 'red' if CAGR is negative, else 'green'
                 # Update layout for the current chart
                 fig.update_layout(
-                    title=f'{metric} {"QoQ" if is_quarterly else "YoY"}',
+                    title=f"{metric} {'QoQ' if is_quarterly else 'YoY'}<br>{'     CQGR' if is_quarterly else 'CAGR'}: <span style=\"color: yellow;\">{cagr:.2%}</span>",
                     title_x=0.3,  # Set the title's horizontal position to the center
                     xaxis=dict(title=''),
                     yaxis=dict(title='% Growth'),
                     width=800,  # Adjust the width of each chart as needed
                     height=400,  # Adjust the height of each chart as needed
-                    font=dict(size=15),
+                    font=dict(size=15, color='black'),
                     legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=1),
                 )
 
                 # Plot the current chart
                 st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+
+                # # Display the CAGR value with reduced space
+                # st.write(f"<div style='text-align: center; margin-top: -45px;'>CAGR for {metric}: {cagr:.2%}</div>",
+                #          unsafe_allow_html=True)
+
+
             else:
                 st.write("Data is empty for metric:", metric)
 
 
 
 
+
     # Define colors for each metric
-    line_colors = ['green', 'orange']
+    line_colors = ['#5483b3', '#c1e8ff']
 
     # Create a new figure for the line chart
     line_fig = go.Figure()
@@ -502,6 +583,20 @@ with col1:
 
     # Iterate over each metric and create a chart
     for metric, col in zip(metrics, [col1, col2]):
+        # Check if start and end values have different signs
+        if (data[metric].iloc[0] < 0 and data[metric].iloc[-1] > 0) or (data[metric].iloc[0] > 0 and data[metric].iloc[-1] < 0):
+            # Calculate CAGR for the current metric
+            start_value = abs(data[metric].iloc[0])
+            end_value = abs(data[metric].iloc[-1])
+            num_periods = len(data) - 1
+            cagr = ((end_value / start_value) ** (1 / num_periods)) - 1
+        else:
+            # Regular CAGR calculation
+            start_value = data[metric].iloc[0]
+            end_value = data[metric].iloc[-1]
+            num_periods = len(data) - 1
+            cagr = ((end_value / start_value) ** (1 / num_periods)) - 1
+
         # Create a new chart for the current metric
         with col:
             if not data_percentage_change_df.empty:
@@ -511,46 +606,49 @@ with col1:
                 # Add trace for the current metric
                 fig.add_trace(go.Scatter(
                     x=pd.to_datetime(data_percentage_change_df.index).strftime(
-                        '%Y-%m-%d' if is_quarterly else '%Y-%m'),
+                        '%Y-%m-%d' if is_quarterly else '%YY'),
                     y=data_percentage_change_df[metric],
                     mode='lines',
                     name=metric,
-                    line=dict(color=line_colors[metrics.index(metric)]),  # Assign color from the list
+                    line=dict(color=line_colors[metrics.index(metric)], width=3)  # Assign color from the list
                 ))
 
                 # Add annotations for each point on the line
                 for index, row in data_percentage_change_df.iterrows():
                     index_dt = pd.to_datetime(index)  # Convert index to datetime object
                     fig.add_annotation(
-                        x=index_dt.strftime('%Y-%m-%d' if is_quarterly else '%Y-%m'),
+                        x=index_dt.strftime('%Y-%m-%d' if is_quarterly else '%YY'),
                         # Use specific x-value for each data point
                         y=row[metric],  # y-coordinate of the annotation
                         text=f"{row[metric]:.2f}%",  # text to display (format as needed)
                         showarrow=False,  # don't show an arrow
-                        font=dict(color='white'),  # color of the annotation text
                         xanchor='right',  # anchor point for x-coordinate
                         yanchor='middle',  # anchor point for y-coordinate
                         align='left',  # alignment of the annotation text
+                        bgcolor='yellow',
                         xshift=5,  # horizontal shift of the annotation
                         yshift=0,  # vertical shift of the annotation
                     )
+                # Define the color based on CAGR value
+                color = 'red' if cagr < 0 else 'yellow'  # Change to 'red' if CAGR is negative, else 'green'
 
                 # Update layout for the current chart
                 fig.update_layout(
-                    title=f'{metric} {"QoQ" if is_quarterly else "YoY"}',
+                    title=f"{metric} {'QoQ' if is_quarterly else 'YoY'}<br>{'    CQGR' if is_quarterly else 'CAGR'}: <span style=\"color: {color};\">{cagr:.2%}</span>",
                     title_x=0.3,  # Set the title's horizontal position to the center
                     xaxis=dict(title=''),
                     yaxis=dict(title='% Growth'),
                     width=800,  # Adjust the width of each chart as needed
                     height=400,  # Adjust the height of each chart as needed
-                    font=dict(size=15),
+                    font=dict(size=15, color='black'),
                     legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=1),
                 )
 
                 # Plot the current chart
                 st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
-            else:
-                st.write("Data is empty for metric:", metric)
+
+
+
 
 
 
@@ -579,6 +677,7 @@ with col1:
         name='Basic EPS',
         text=["{:,.2f}$".format(value) for value in basic_eps.values],  # Add $ and format to two decimal places
         textposition='auto',
+        marker=dict(color='blue')
     ))
 
     # Add Diluted EPS bars
@@ -591,12 +690,13 @@ with col1:
         name='Diluted EPS',
         text=["{:,.2f}$".format(value) for value in diluted_eps.values],  # Add $ and format to two decimal places
         textposition='auto',
+        marker=dict(color='#0080ff')  # Medium blue color
     ))
 
     # Update layout
     fig.update_layout(
         title='Basic EPS vs Diluted EPS',
-        title_x=0.4,
+        title_x=0.38,
         xaxis=dict(title='' if is_quarterly else ''),
         yaxis=dict(title='EPS Value'),
         barmode='group',
@@ -636,6 +736,7 @@ with col2:
         name='EBITDA',
         text=ebitda.values,
         textposition='auto',
+        marker=dict(color='blue')
 
     ))
 
@@ -649,7 +750,7 @@ with col2:
         name='EBIT',
         text=ebit.values,
         textposition='auto',
-        # marker=dict(color='rgba(0, 0, 255, 0.5)')  # Medium blue color
+        marker=dict(color='#0080ff')  # Medium blue color
     ))
 
     # Add Net Income bars
@@ -663,7 +764,7 @@ with col2:
         name='Net Income',
         text=net_income.values,
         textposition='auto',
-        marker=dict(color='green')  # Medium blue color
+        marker=dict(color='#c1e8ff')  # Medium blue color
     ))
 
     # Update layout
