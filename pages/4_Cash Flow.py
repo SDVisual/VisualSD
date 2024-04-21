4ce965bc46cb3d40bea0ab8e6f891bdc079b3c25
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-04-21 09:32:37 +0300

    Update 4_Cash Flow.py

--- pages/4_Cash Flow.py
+++ pages/4_Cash Flow.py
@@ -87,8 +87,9 @@ st.session_state.selected_ticker_index = st.session_state.valid_tickers.index(ti
 st.sidebar.info("- For the best experience, maximize your screen.")
 st.sidebar.info("- Close side bar for better visualization.")
 st.sidebar.info("- Recommended dark mode in setting menu.")
-st.sidebar.markdown("&copy;Dan Oren. All rights reserved.", unsafe_allow_html=True)
+st.sidebar.info("- This app version is less suitable for stocks in the finance industry")
 
+st.sidebar.markdown("&copy;VisualSD by Dan Oren. All rights reserved.", unsafe_allow_html=True)
 
 
 
