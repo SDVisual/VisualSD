9290cbfc4ae4daea18b85faec19f753d4d51163b
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-06-01 16:35:15 +0300

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -270,7 +270,6 @@ with col2:
     button_container = st.container()
 
 
-
     with button_container:
         button_spacing = 1  # Adjust spacing between buttons
         st.write('<style>div.row-widget.stHorizontal {flex-wrap: nowrap;}</style>', unsafe_allow_html=True)
