4ea3295316f0582800a7d5c605849ab8985640e4
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-10-06 13:13:29 +0300

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -52,15 +52,15 @@ with col1:
 
     # Check if the entered symbol is valid
     
-    # historical_data = yf.Ticker(new_symbol).history(period='1d')
+    historical_data = yf.Ticker(new_symbol).history(period='1d')
 
-    try:
-        historical_data = yf.Ticker(new_symbol).history(period='1d')
-        st.write(historical_data)  # Log the fetched historical data
-        income_statement = yf.Ticker(new_symbol).income_stmt
-        st.write(income_statement)  # Log the fetched income statement
-    except Exception as e:
-        st.error(f"Failed to fetch data for {new_symbol}. Error: {e}")
+    # try:
+    #     historical_data = yf.Ticker(new_symbol).history(period='1d')
+    #     st.write(historical_data)  # Log the fetched historical data
+    #     income_statement = yf.Ticker(new_symbol).income_stmt
+    #     st.write(income_statement)  # Log the fetched income statement
+    # except Exception as e:
+    #     st.error(f"Failed to fetch data for {new_symbol}. Error: {e}")
 
     
     income_statement = yf.Ticker(new_symbol).income_stmt
