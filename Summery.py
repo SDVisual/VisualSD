ffba9d8c333449faffac124d356e9461052a06e8
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-10-06 13:47:31 +0300

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -77,7 +77,7 @@ with col1:
 
 
     
-    # income_statement = yf.Ticker(new_symbol).income_stmt
+    income_statement = yf.Ticker(new_symbol).income_stmt
 
     if new_symbol != DEFAULT_SYMBOL and historical_data.empty or income_statement.empty:
 
