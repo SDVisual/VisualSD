b6c3c8f9c707250d2d5f0c3d4f5dee4fd041c31f
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-10-14 15:46:17 +0300

    Update 2_Income_Statements.py

--- pages/2_Income_Statements.py
+++ pages/2_Income_Statements.py
@@ -46,12 +46,11 @@ if not new_symbol or new_symbol.isspace():
 
 
 # Check if the entered symbol is valid
-# historical_data = yf.Ticker(new_symbol).history(period='1d')
+historical_data = yf.Ticker(new_symbol).history(period='2d')
 income_statement = yf.Ticker(new_symbol).income_stmt
 
-# if new_symbol != DEFAULT_SYMBOL and historical_data.empty or income_statement.empty:
+if new_symbol != DEFAULT_SYMBOL and historical_data.empty or income_statement.empty:
 
-if new_symbol != DEFAULT_SYMBOL and income_statement.empty:
     st.error("Invalid symbol. Please enter Only Stocks symbols.")
 
 else:
