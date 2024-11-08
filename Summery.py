c888b555657ea31e4d0800df6db1f2b68bec3a61
VisualSD <164010355+SDVisual@users.noreply.github.com>
2024-11-08 19:25:30 +0200

    Update Summery.py

--- Summery.py
+++ Summery.py
@@ -321,7 +321,7 @@ with col2:
 
     # Fetch data based on the selected time period or default to '1Y'
     selected_time_period = st.session_state.get('selected_time_period', '3M')
-    # df_ticker = yf.download(ticker, period='max').reset_index()
+    df_ticker = yf.download(ticker, period='max').reset_index()
 
     # Create the ticker instance
     ticker_data = yf.Ticker(ticker)
@@ -331,8 +331,8 @@ with col2:
     # st.write(df_ticker)
 
 
-        # Fetch the historical data
-    df_ticker = ticker_data.history(period="max")
+    #     # Fetch the historical data
+    # df_ticker = ticker_data.history(period="max")
     
     # Check if there's an extra header row with the symbol and drop it
     if isinstance(df_ticker.columns, pd.MultiIndex):
